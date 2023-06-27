65b4a05cbb50af2fbb39548ea68c36cbfe741dec
mikhail-na <115450336+mikhail-na@users.noreply.github.com>
2023-06-27 12:07:26 +0200

    commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>First react-native App!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    fontSize: "32px",
+    
+    fontWeight: "700",
+    color: "rgb(188, 188, 14)",
+  },
+});
